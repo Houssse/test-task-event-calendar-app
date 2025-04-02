944bc3c3cf1ab423a4f354e7c235e5f0a334ba52
Yuriy Nikolaev <yuriy.nikolaev.98@mail.ru>
2025-04-02 10:05:29 +0300

    add i18n devise

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootsnap', require: false
 # gem "image_processing", "~> 1.2"
 
 gem 'devise'
+gem 'devise-i18n-views'
 gem 'simple_form'
 gem 'slim-rails'
 
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ Bundler.require(*Rails.groups)
 
 module TestTaskEventCalendarApp
   class Application < Rails::Application
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
 
